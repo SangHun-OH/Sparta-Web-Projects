e7ce8d2bb77c6b9d54eca6a3306de7dd69b9217d
SangHun-OH <oshosh12@gmail.com>
2021-07-04 03:21:44 +0900

    Book_review

--- bookreview/app.py
+++ bookreview/app.py
@@ -13,28 +13,28 @@ def home():
 ## API 역할을 하는 부분
 @app.route('/review', methods=['POST'])
 def write_review():
-    sample_receive = request.form['sample_give']
-    print(sample_receive)
-    return jsonify({'msg': '이 요청은 POST!'})
+
+    title_receive = request.form['title_give']
+    author_receive = request.form['author_give']
+    review_receive = request.form['review_give']
+
+    # dictionary 생성
+    doc = {
+        'title': title_receive,
+        'author': author_receive,
+        'review': review_receive
+    }
+
+    # 저장
+    db.bookreview.insert_one(doc)
+    return jsonify({'msg': '저장완료'})
 
 
 @app.route('/review', methods=['GET'])
 def read_reviews():
-    title_receive = request.args.get('title_give')
-    author_receive = request.args.get('author_give')
-    review_receive = request.args.get('review_give')
+    reviews = list(db.bookreview.find({},{'_id': False}))
+    return jsonify({'all_reviews':reviews})
 
-    #dictionary 생성
-    doc = {
-        'title':title_receive,
-        'author':author_receive,
-        'review':review_receive
-    }
-
-    #저장
-    db.bookreview.insert_one(doc)
-
-    return jsonify({'msg': '저장완료'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
